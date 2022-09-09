6ec298b4d071cb9a7566863e6d170c30b474abec
Ekene Ezeasor <ezeasorekene@gmail.com>
2022-09-09 07:59:12 +0100

    Low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main() {
-   puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+/* This script prints using puts */
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return 0;
 }
