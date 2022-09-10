8a3eda5ecfc40c261fac80e25da33f6df6f1276e
Ekene Ezeasor <ezeasorekene@gmail.com>
2022-09-10 06:01:16 +0100

    Low level programming - 0x01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,30 +4,31 @@
 /**
  * main - Entry point
  *
- * Description: This program will assign a random number to the variable n each time it is executed.
+ * Description: Assign a random number to n each time it is executed
  *
  * Return: Always 0 (Success)
  */
 int
-main (void)
+main(void)
 {
-  int n;
+	int n;
 
-  srand (time (0));
-  n = rand () - RAND_MAX / 2;
-  /* your code goes there */
-  if (n > 0)
-    {
-      printf("%d is positive\n", n);
-    }
-  else if (n == 0)
-    {
-      printf("%d is zero\n", n);
-    }
-  else
-    {
-      printf("%d is negative\n", n);
-    }
-  return (0);
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
 
